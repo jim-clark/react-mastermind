ad1344a87054fea7f9d1dcac3abfa1a516d1a2b9
jim-clark <email@jim-clark.com>
2018-11-03 15:27:17 -0700

    Add skeleton components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import GameBoard from './components/GameBoard/GameBoard';
+import ColorPicker from './components/ColorPicker/ColorPicker';
+import GameTimer from './components/GameTimer/GameTimer';
+import NewGameButton from './components/NewGameButton/NewGameButton';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <header className="App-header">React Mastermind</header>
+        <div className="flex-h">
+          <GameBoard />
+          <div>
+            <ColorPicker />
+            <GameTimer />
+            <NewGameButton />
+          </div>
+        </div>
+        <footer className="component">footer</footer>
       </div>
     );
   }
